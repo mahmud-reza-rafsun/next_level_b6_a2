959b0ddaa4b292144472f07144b012a30f14716c
Mahmud Reza Rafsun <97410422+mahmud-reza-rafsun@users.noreply.github.com>
2025-12-07 19:39:42 +0600

    Delete dist directory

--- dist/modules/auth/auth.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.auhtRouter = void 0;
-const express_1 = require("express");
-const auth_controller_1 = require("./auth.controller");
-const router = (0, express_1.Router)();
-router.post("/signin", auth_controller_1.authController.signinUser);
-exports.auhtRouter = router;
--- dist/modules/vehicles/vehicle.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.vehicleRouter = void 0;
-const express_1 = require("express");
-const vehicle_controller_1 = require("./vehicle.controller");
-const router = (0, express_1.Router)();
-router.post("/", vehicle_controller_1.vehicleController.createVehicle);
-exports.vehicleRouter = router;
